d49322c6921ebf72ae6b294d4ab6368129aafe2e
alexlee <alexlee910116@gmail.com>
2022-01-09 22:49:59 +0900

    判断用户是否重复的实现

--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
+
 
 def log(request):
     import logging
@@ -28,5 +29,5 @@ def log(request):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('log/', log),
+    path('', include('apps.users.urls')),
 ]
