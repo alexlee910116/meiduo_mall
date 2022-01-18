bb6fff666680b341e87b6665791e004bd59a030c
alexlee <alexlee910116@gmail.com>
2022-01-18 12:41:21 +0900

    首页用户信息退出功能

--- meiduo_mall/apps/users/views.py
+++ meiduo_mall/apps/users/views.py
@@ -106,3 +106,26 @@ class LoginView(View):
         # 为了首页显示用户信息
         response.set_cookie('username', username)
         return response
+
+
+from django.contrib.auth import logout
+
+
+class LogoutView(View):
+    def delete(self, request):
+        # 1.删除session信息
+        logout(request)
+        response = JsonResponse({'code': 0, 'errmsg': 'ok'})
+        # 2.删除cookie信息
+        response.delete_cookie('username')
+        return response
+
+
+# 用户中心必须为登录用户
+# LoginRequiredMixin 未登录用户会返回重定向
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class CenterView(LoginRequiredMixin, View):
+    def get(self, request):
+        return
