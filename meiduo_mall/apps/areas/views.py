31f7fda085bcf70a66331ef791dd2c8b519cf965
alexlee <alexlee910116@gmail.com>
2022-02-02 12:03:55 +0900

    缓存的功能实现

--- meiduo_mall/apps/areas/views.py
+++ meiduo_mall/apps/areas/views.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
@@ -16,17 +17,26 @@ from apps.areas.models import Area
 class AreaView(View):
 
     def get(self, request):
-        # 1. 查询省份信息
-        provinces = Area.objects.filter(parent=None)
-        # 2.查询结果表
-        # 将对象转换为字典数据
-        province_list = []
-        for province in provinces:
-            province_list.append({
-                'id': province.id,
-                'name': province.name
-            })
-        # 3.返回响应
+        # 先查询缓存数据
+        province_list = cache.get('province')
+        # 如果没有缓存，则查询数据库，并缓存
+        if province_list is None:
+            # 1. 查询省份信息
+            provinces = Area.objects.filter(parent=None)
+            # 2.查询结果表
+            # 将对象转换为字典数据
+            province_list = []
+            for province in provinces:
+                province_list.append({
+                    'id': province.id,
+                    'name': province.name
+                })
+
+            # 保存缓存数据
+            # cache.set(key, value, expire)
+            cache.set('province', province_list, 24 * 3600)
+
+            # 3.返回响应
         return JsonResponse({'code': 0, 'errmsg': 'ok', 'province_list': province_list})
 
 
@@ -43,16 +53,24 @@ class AreaView(View):
 class SubAreaView(View):
 
     def get(self, request, id):
-        # Area.objects.filter(parent=id)
-        # Area.objects.filter(parent_id=id)
+        # 获取缓存数据
+        data_list = cache.get('city:%s' % id)
+        if data_list is None:
+            # Area.objects.filter(parent=id)
+            # Area.objects.filter(parent_id=id)
 
-        up_level = Area.objects.get(id=id)
-        down_level = up_level.subs.all()
-        data_list = []
-        for item in down_level:
-            data_list.append({
-                'id': item.id,
-                'name': item.name
-            })
+            up_level = Area.objects.get(id=id)
+            down_level = up_level.subs.all()
+            data_list = []
+            for item in down_level:
+                data_list.append({
+                    'id': item.id,
+                    'name': item.name
+                })
+
+            # 缓存数据
+            cache.set('city:%s' % id, data_list, 24 * 3600)
 
         return JsonResponse({'code': 0, 'errmsg': 'ok', 'sub_data': {'subs': data_list}})
+
+# 不经常发生变化的数据 最好缓存到redis，减少数据库查询
